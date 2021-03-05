7c763092145e1033d1073a0fa54933644c5e8080
Jeff Ashton <JeffAshton@users.noreply.github.com>
2021-03-04 20:52:50 -0500

    Adding kinesis record wrapper

---
+++ plugins/outputs/d2l_kinesis/kinesisRecord.go
@@ -0,0 +1,27 @@
+package d2lkinesis
+
+import (
+	"github.com/aws/aws-sdk-go/service/kinesis"
+)
+
+func createKinesisRecord(
+	entry *kinesis.PutRecordsRequestEntry,
+	metrics int,
+) kinesisRecord {
+
+	// Partition keys are included in the request size calculation.
+	// This is assuming partition keys are ASCII.
+	requestSize := len(entry.Data) + len(*entry.PartitionKey)
+
+	return kinesisRecord{
+		Entry:       entry,
+		Metrics:     metrics,
+		RequestSize: requestSize,
+	}
+}
+
+type kinesisRecord struct {
+	Entry       *kinesis.PutRecordsRequestEntry
+	Metrics     int
+	RequestSize int
+}
